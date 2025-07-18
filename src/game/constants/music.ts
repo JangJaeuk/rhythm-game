4c02312efe9c0557eefb969fa6935810bcabf36a
장재욱 <jju1221@jainwon.com>
2025-07-18 16:07:47 +0900

    새로운 음악 삽입

--- src/game/constants/music.ts
+++ src/game/constants/music.ts
@@ -9,5 +9,13 @@ export const MUSIC_LIST: Music[] = [
       duration: 29,
       audioFile: "./src/assets/jingle-bells.mp3",
       thumbnail: ""
+    },
+    {
+        id: "a-small-miracle",
+        title: "작은 기적",
+        difficulty: 2,
+        duration: 77,
+        audioFile: "./src/assets/a-small-miracle.mp3",
+        thumbnail: ""
     }
   ] as const;
