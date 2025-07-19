874200ff6c13fa8a9b1e464c4dc100efac171380
장재욱 <jju1221@jainwon.com>
2025-07-19 13:48:56 +0900

    asset 파일 경로 수정

--- src/game/constants/music.ts
+++ src/game/constants/music.ts
@@ -1,5 +1,3 @@
-import aSmallMiracleThumbnail from "../../assets/a-small-miracle/thumbnail.jpg";
-import jingleBellsThumbnail from "../../assets/jingle-bells/thumbnail.png";
 import { Music } from "../types";
 
 // 음악 목록
@@ -9,15 +7,15 @@ export const MUSIC_LIST: Music[] = [
         title: "징글벨",
         difficulty: 1,
         duration: 29,
-        audioFile: "./src/assets/jingle-bells/music.mp3",
-        thumbnail: jingleBellsThumbnail,
+        audioFile: "/assets/jingle-bells/music.mp3",
+        thumbnail: "/assets/jingle-bells/thumbnail.png",
     },
     {
         id: "a-small-miracle",
         title: "작은 기적",
         difficulty: 3,
         duration: 77,
-        audioFile: "./src/assets/a-small-miracle/music.mp3",
-        thumbnail: aSmallMiracleThumbnail,
+        audioFile: "/assets/a-small-miracle/music.mp3",
+        thumbnail: "/assets/a-small-miracle/thumbnail.jpg",
     }
-  ] as const;
+] as const;
